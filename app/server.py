e16ab6b41e1bd8f67b937c544cac3bb33a4f9a91
Shaun Jackson <sjackson95@gmail.com>
2019-01-26 19:34:24 -0800

    took code from failed function and modified it to use cpu only

--- app/server.py
+++ app/server.py
@@ -31,7 +31,17 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
-    learn = load_learner(path, export_file_name)
+    state = torch.load(open(Path(path)/fname, map_location = 'cpu'))
+    model = state.pop('model')
+    src = LabelLists.load_state(path, state.pop('data'))
+    if test is not None: src.add_test(test)
+    data = src.databunch()
+    cb_state = state.pop('cb_state')
+    clas_func = state.pop('cls')
+    res = clas_func(data, model, **state)
+    res.callback_fns = state['callback_fns'] #to avoid duplicates
+    res.callbacks = [load_callback(c,s, res) for c,s in cb_state.items()]
+    learn = res
     return learn
 
 loop = asyncio.get_event_loop()
